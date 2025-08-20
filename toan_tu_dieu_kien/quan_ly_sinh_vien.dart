7c7e3a1814b566a62ead42e4d4d963765538d507
vuphananh92 <vu.phananh92@gmail.com>
2025-08-20 09:13:07 +0000

    Nộp bài tập Dart trong thư mục toan_tu_dieu_kien

---
+++ toan_tu_dieu_kien/quan_ly_sinh_vien.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  // Nhập thông tin sinh viên
+  stdout.write('Nhập họ tên');
+  String hoTen = stdin.readLineSync()!; /* Đọc dòng chữ người dùng nhập vào */
+  // Nhập số giờ làm việc
+  stdout.write('Nhập số giờ làm việc');
+  int giolam = int.tryParse(stdin.readLineSync()!) ?? 0;
+}
