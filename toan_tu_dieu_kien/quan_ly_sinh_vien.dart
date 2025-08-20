279eb1a9b2166bdc3c010b01bc45bb3da2ef88b1
vuphananh92 <vu.phananh92@gmail.com>
2025-08-20 09:21:59 +0000

    Nộp bài tập Dart trong thư mục toan_tu_dieu_kien

--- toan_tu_dieu_kien/quan_ly_sinh_vien.dart
+++ toan_tu_dieu_kien/quan_ly_sinh_vien.dart
@@ -6,5 +6,5 @@ void main() {
   String hoTen = stdin.readLineSync()!; /* Đọc dòng chữ người dùng nhập vào */
   // Nhập số giờ làm việc
   stdout.write('Nhập số giờ làm việc');
-  int giolam = int.tryParse(stdin.readLineSync()!) ?? 0;
+  int giolam = int.tryParse(stdin.readLineSync()!) ?? 0;/* Đọc số giờ người dùng nhập vào từ bàn phím */
 }
