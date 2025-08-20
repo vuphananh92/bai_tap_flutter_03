9128107234cc2e8e8a1cba7baaa4863fc6d5b3ce
vuphananh92 <vu.phananh92@gmail.com>
2025-08-20 12:13:34 +0000

    Bài tập 1: quản lý sinh viên

--- toan_tu_dieu_kien/quan_ly_sinh_vien.dart
+++ toan_tu_dieu_kien/quan_ly_sinh_vien.dart
@@ -7,7 +7,25 @@ void main() {
   // Nhập số giờ làm việc
   stdout.write('Nhập số giờ làm việc');
   int giolam = int.tryParse(stdin.readLineSync()!) ?? 0;/* Đọc số giờ người dùng nhập vào từ bàn phím */
-  // xóa
+  // Nhập lương mỗi giờ
+  stdout.write('Nhập lương mỗi giờ');
+  double luongMoiGio = double.tryParse(stdin.readLineSync()!) ?? 0;
 
+  double tongLuong = giolam * luongMoiGio;
+  if (giolam > 40) {
+    tongLuong += tongLuong * 0.2;
+  }
+  double thue = 0;
+  if (tongLuong > 10000000) {
+    thue = tongLuong * 0.1;
+  } else if (tongLuong >= 7000000) {
+    thue = tongLuong * 0.5;
+  } 
+  double luongThucLanh = tongLuong - thue;
+  print('Kết quả:');
+  print('Họ và tên: $hoTen');
+  print('Lương trước thuê: $tongLuong');
+  print('Thuế thu nhập: $thue');
+  print('Lương thực lãnh: $luongThucLanh');
 }
 
